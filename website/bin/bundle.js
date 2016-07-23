27c6a9e014983c2b41d8b7b6fb66f58578255217
Wenzhe Peng <Wenzhe Peng>
2016-07-23 16:22:15 +0800

    update

--- website/bin/bundle.js
+++ website/bin/bundle.js
@@ -141,6 +141,17 @@
 	        $( window ).resize(function() {
 	            $(".project_icon").css("height", $(".project_icon").css("width"));
 	        });
+
+	        d3.selectAll(".project_icon").on("mouseover",function(){
+	            d3.select(this).select(".project_info").style("opacity","1");
+	            d3.select(this).select(".project_cover").style("opacity","1");
+
+	        }).on("mouseout",function(){
+	            d3.select(this).select(".project_info").style("opacity","0");
+	            d3.select(this).select(".project_cover").style("opacity","0");
+
+	        });
+
 	    }
 	}
 
--- website/src/modules/myui.js
+++ website/src/modules/myui.js
@@ -13,5 +13,16 @@ MyUI.prototype = {
         $( window ).resize(function() {
             $(".project_icon").css("height", $(".project_icon").css("width"));
         });
+
+        d3.selectAll(".project_icon").on("mouseover",function(){
+            d3.select(this).select(".project_info").style("opacity","1");
+            d3.select(this).select(".project_cover").style("opacity","1");
+
+        }).on("mouseout",function(){
+            d3.select(this).select(".project_info").style("opacity","0");
+            d3.select(this).select(".project_cover").style("opacity","0");
+
+        });
+
     }
 }
