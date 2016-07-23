3fb18df1d61e07fb8229998ff7bb9b3fb3b703d1
Wenzhe Peng <Wenzhe Peng>
2016-07-22 22:49:53 -0400

    ..

--- website/bin/bundle.js
+++ website/bin/bundle.js
@@ -136,10 +136,10 @@
 	    initcontrol: function(){
 	        var myobj = this;
 
-	        d3.selectAll(".project_icon").style("height", d3.select(".project_icon").style("width"));
+	        $(".project_icon").css("height", $(".project_icon").css("width"));
 
 	        $( window ).resize(function() {
-	            d3.selectAll(".project_icon").style("height", d3.select(".project_icon").style("width"));
+	            $(".project_icon").css("height", $(".project_icon").css("width"));
 	        });
 	    }
 	}
